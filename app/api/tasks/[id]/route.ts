e85aff1307748634edd555aae0049408ac3c319f
Hussein Najah <husseinnajah123@gmail.com>
2024-10-25 17:06:03 +0300

    Refactor task update route to handle PUT requests

--- app/api/tasks/[id]/route.ts
+++ app/api/tasks/[id]/route.ts
@@ -26,3 +26,52 @@ export async function DELETE(
     return NextResponse.json({ error: "Error deleting task", status: 500 });
   }
 }
+
+export async function PUT(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
+  try {
+    const { userId } = auth();
+    const { id } = params;
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const { title, description, date, completed, important } = await req.json();
+
+    if (!title || !description || !date) {
+      return NextResponse.json({
+        error: "Missing required fields",
+        status: 400,
+      });
+    }
+
+    if (title.length < 3) {
+      return NextResponse.json({
+        error: "Title must be at least 3 characters long",
+        status: 400,
+      });
+    }
+
+    const task = await prisma.task.update({
+      where: {
+        id: id,
+      },
+      data: {
+        title,
+        description,
+        date,
+        isCompleted: completed,
+        isImportant: important,
+        userId,
+      },
+    });
+
+    return NextResponse.json(task);
+  } catch (error) {
+    console.log("ERROR UPDATING TASK: ", error);
+    return NextResponse.json({ error: "Error updating task", status: 500 });
+  }
+}
